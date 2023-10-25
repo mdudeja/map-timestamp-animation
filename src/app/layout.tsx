5cbb846035d52d93522375d5a549dc128c65929a
mridul <mridul20384@gmail.com>
2023-10-25 12:48:09 +0530

    og icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,17 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "The signatures as they came: time and location",
   description: "SC Judgment on #MarriageEquality #LGBTQIA",
+  icons: ["/images/icon.ico", "/images/icon.png"],
+  openGraph: {
+    images: [
+      {
+        url: "/images/icon.png",
+        width: 200,
+        height: 200,
+        alt: "The signatures as they came: time and location",
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
